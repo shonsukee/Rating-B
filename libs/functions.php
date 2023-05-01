1785ee917424e37c71e4661c370cb370f2ef0681
inoue <inoueshoei.313.9720@gmail.com>
2023-05-01 13:40:40 +0900

    change database3

--- libs/functions.php
+++ libs/functions.php
@@ -35,22 +35,17 @@ function new_PDO() //PDOインスタンスを返却
 {
 	$dbUrl = getenv('JAWSDB_URL');
 	$pattern = '/mysql:\/\/(.*):(.*)@(.*):(.*)\/(.*)/';
-	print_r($dbUrl);
-	print_r($pattern);
 	preg_match($pattern, $dbUrl, $params);
 	$database = $params[5];
 	$username = $params[1];
 	$password = $params[2];
 	$host = $params[3];
 	$port = $params[4];
-
+	
 	$pdo = new PDO("mysql:host=$host;port=$port;dbname=$database", $username, $password, [
-	    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-	    PDO::MYSQL_ATTR_INIT_COMMAND => "SET sql_mode='STRICT_TRANS_TABLES'",
-		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES => false
+		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+		PDO::MYSQL_ATTR_INIT_COMMAND => "SET sql_mode='STRICT_TRANS_TABLES'",
 	]);
-
 
 	$tableName = "comments";
 	$columns = "id INT(11) NOT NULL AUTO_INCREMENT,
